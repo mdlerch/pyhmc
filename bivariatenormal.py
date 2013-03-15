903ef91ac938cd69ff2d5d3f2130b331fa08a792
Michael Lerch <mdlerch@gmail.com>
2013-03-15 15:37:59 -0600

    use generalized coordinates

--- bivariatenormal.py
+++ bivariatenormal.py
@@ -2,18 +2,18 @@ import numpy as np
 import math
 
 # density
-def density(x):
-	pdf = math.exp((x[0] - 0) ** 2 / 2) * math.exp((x[1] - 0) ** 2 / 2)
+def density(q):
+	pdf = math.exp((q[0] - 0) ** 2 / 2) * math.exp((q[1] - 0) ** 2 / 2)
 	return(pdf)
 
 # hamiltonian
-def hamiltonian(x, v, m):
-	E = - math.log(density(x)) + 1./2. * m * np.dot(v, v)
+def hamiltonian(q, p):
+	E = - math.log(density(q)) + 1./2. *  np.dot(p, p)
 	return(E)
 
 # Calculate the derivative of the potential field
-def deriv(x):
-	dx = - x[0]
-	dy = - x[1]
+def deriv(q):
+	dx = - q[0]
+	dy = - q[1]
 	out = np.array([dx, dy])
 	return(out)
--- pyhmc.py
+++ pyhmc.py
@@ -4,12 +4,11 @@ import csv
 from bivariatenormal import *
 
 # Iteration
-def leapfrog(x, p, eps, m):
-	p2 = p + eps / 2. * deriv(x)
-	x2 = x + eps * p2 / m
-	p2 = p2 + eps / 2. * deriv(x2)
-	v2 = p2 / float(m)
-	return( x2, v2 )
+def leapfrog(q, p, eps):
+	p2 = p + eps / 2. * deriv(q)
+	q2 = q + eps * p2
+	p2 = p2 + eps / 2. * deriv(q2)
+	return(q2, p2)
 
 # number of leapfrog steps
 L = 50
@@ -25,33 +24,31 @@ I = 5000
 # initialize and initial conditions
 
 # samples
-xm = np.zeros([I,2])
+qm = np.zeros([I,2])
 
 # leapfrog sequence
-x = np.zeros([L, 2])
-v = np.zeros([L, 2])
-x[L - 1] = [3, 0]
-v[L - 1] = [0, .05]
-
+q = np.zeros([L, 2])
+p = np.zeros([L, 2])
+q[L - 1] = [3, 0]
 
 for i in range(I-1):
-	x[0] = x[L - 1]
-	v0 = v[L -1]
-	v[0] = np.random.multivariate_normal([0, 0], [[1,0], [0,1]]) / m
+	q[0] = q[L - 1]
+	p0 = p[L -1]
+	p[0] = np.random.multivariate_normal([0, 0], [[1,0], [0,1]])
 
 	for step in range(L-1):
-		x[step + 1], v[step + 1] = leapfrog(x[step], m * v[step], epsilon, m)
+		q[step + 1], p[step + 1] = leapfrog(q[step], p[step], epsilon)
 
 	# gibbs acceptance/rejection
-	Ei = hamiltonian(x[0], v0, m)
-	Ef = hamiltonian(x[L-1], v[L-1], m)
+	Ei = hamiltonian(q[0], p0)
+	Ef = hamiltonian(q[L-1], q[L-1])
 	alpha = min(1, math.exp(- Ef + Ei))
 	if np.random.random() < alpha:
-		xm[i + 1] = x[L - 1]
+		qm[i + 1] = q[L - 1]
 	else:
-		xm[i + 1] = xm[i]
+		qm[i + 1] = qm[i]
 
 	# np.savetxt("./output/outputx"+str(i)+".txt",x,delimiter=" ")
 
-np.savetxt("./output/output.txt", xm, delimiter=" ")
+np.savetxt("./output/output.txt", qm, delimiter=" ")
 
