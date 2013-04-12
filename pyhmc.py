0b6d3b1ff552faf5edf5e895bea0f2d97eb5a9e4
Michael Lerch <mdlerch@gmail.com>
2013-04-12 15:59:13 -0600

    add input file

--- pyhmc.py
+++ pyhmc.py
@@ -6,6 +6,7 @@ import numpy as np
 import sys
 
 import bivariatenormal
+import binom
 from iterate import sample
 
 # {{{ parse
@@ -20,31 +21,33 @@ parser.add_argument("--demo",
 ## likelihood
 parser.add_argument("likelihood",
                     nargs="?",
-                    choices=["normal", "binomial"],
-                    metavar="likelihood")
+                    choices=["binomial"],
+                    metavar="likelihood",
+                    help="Likelihood to use")
 ## prior
-parser.add_argument("prior",
-                    nargs="?",
-                    choices=["flat1", "flat2"],
-                    metavar="prior")
+# parser.add_argument("prior",
+                    # nargs="?",
+                    # choices=["flat1", "flat2"],
+                    # metavar="prior",
+                    # help="prior to use")
 
 # sampler parameters
 ## number of samples
 parser.add_argument("-S",
                     type=int,
-                    help="number of samples",
-                    default=1000)
+                    default=1000,
+                    help="number of samples")
 ## number of itegrator steps
 parser.add_argument("-L",
                     type=int,
-                    help="number of integrator steps between samples",
-                    default=10)
+                    default=10,
+                    help="number of integrator steps between samples")
 ## time step
 parser.add_argument("-E",
                     "--epsilon",
                     type = float,
-                    help="time step",
-                    default=0.1)
+                    default=0.1,
+                    help="time step")
 ## initialize the chain
 parser.add_argument("-I",
                     type=float,
@@ -58,11 +61,14 @@ parser.add_argument("-Y",
                     nargs="+",
                     help="Data. Structure depends on likelihood")
 
+parser.add_argument("--infile",
+                    nargs="+",
+                    help="Data. Structure depends on likelihood")
 # outputs
 ## output file
 parser.add_argument("-O",
-                    help="Output file",
-                    default="output.txt")
+                    default="output.txt",
+                    help="Output file")
 
 # additional options
 ## verbosity (for trouble shooting)
@@ -72,22 +78,30 @@ parser.add_argument("-V",
 
 
 args = parser.parse_args()
+
 try:
 	if not (args.demo or args.likelihood):
 		raise Exception
 except:
 	print("Either demo needs to be turned on" +
-	" or likelihood and prior need to be set\n")
+	" or likelihood needs to be set\n")
 	parser.print_help()
 	sys.exit(1)
 
+######################################
+#  Only demo is currently available  #
+######################################
+
 try:
 	if not args.demo:
-		raise Exception
-	else:
-		sampler = bivariatenormal.bivariateNormal()
+		if args.likelihood == "binomial":
+			sampler = binom.binomial()
+		else:
+			raise Exception
+		pass
 except:
-	print("Currently, only the demo is available\n")
+	print("Currently only binomial is available\n" +
+			"you selected %s" % args.likelihood)
 	parser.print_help()
 	sys.exit(1)
 
@@ -102,6 +116,9 @@ if args.Y:
 	y = np.array(args.Y)
 else:
 	y = sampler.defaulty
+
+if args.infile:
+	y = sampler.readdata(args.infile[0])
 
 # initialize chain
 if args.I:
@@ -122,7 +139,7 @@ outfile = args.O
 print("\n")
 if not args.demo=="demo":
 	print("Likelihood: %s" % args.likelihood)
-	print("Prior: %s" % args.prior)
+	# print("Prior: %s" % args.prior)
 if args.demo=="demo":
 	print("Demo mode: Sampling from a bivariate normal \
 with mean %s and standard deviation 1" % y)
@@ -140,7 +157,6 @@ print("\nWriting results to %s" % outfile)
 ######################
 
 qm = sample(y, S, L, epsilon, init, sampler, verbose)
-
 
 
 #################
